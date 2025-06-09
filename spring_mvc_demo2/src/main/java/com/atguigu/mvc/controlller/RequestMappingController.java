93c1dee49e37999e0b29c1b9ecf3873fa38fd705
ljyGmail <liangjy881@gmail.com>
2025-06-09 23:32:39 +0900

    18 @RequestMapping注解的method属性

--- spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
+++ spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
@@ -2,12 +2,13 @@ package com.atguigu.mvc.controlller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 // @RequestMapping("/hello")
 public class RequestMappingController {
 
-    @RequestMapping(value = {"/testRequestMapping", "/test"})
+    @RequestMapping(value = {"/testRequestMapping", "/test"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String success() {
         return "success";
     }
