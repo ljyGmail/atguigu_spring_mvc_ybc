f50023837f1009c96859dd3c90d7cfffa0176ca8
ljyGmail <liangjy881@gmail.com>
2025-06-09 23:37:31 +0900

    19 @RequestMapping注解结合请求方式的派生注解

--- spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
+++ spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
@@ -1,6 +1,7 @@
 package com.atguigu.mvc.controlller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +11,11 @@ public class RequestMappingController {
 
     @RequestMapping(value = {"/testRequestMapping", "/test"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String success() {
+        return "success";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testGetMapping() {
         return "success";
     }
 }
