0b94357608f988cf47641d7cf0076a2e78f489a4
ljyGmail <liangjy881@gmail.com>
2025-06-09 19:06:41 +0900

    11 SpringMVC HelloWorld: 访问首页

--- spring_mvc_demo1/src/main/java/com/atguigu/mvc/controller/HelloController.java
+++ spring_mvc_demo1/src/main/java/com/atguigu/mvc/controller/HelloController.java
@@ -1,7 +1,15 @@
 package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+
+    // "/" --> /WEB-INF/templates/index.html
+    @RequestMapping("/")
+    public String index() {
+        // 返回视图名称
+        return "index";
+    }
 }
