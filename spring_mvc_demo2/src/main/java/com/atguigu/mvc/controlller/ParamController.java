0b805083f1f56e06a41a849f5d991b19ca228c34
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-10 16:08:39 +0900

    25 回顾原生Servlet获取请求参数

---
+++ spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/ParamController.java
@@ -0,0 +1,16 @@
+package com.atguigu.mvc.controlller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * ClassName: ParamController
+ * Package: com.atguigu.mvc.controlller
+ * Description:
+ *
+ * @Author ljy
+ * @Create 2025. 6. 10. 오후 4:04
+ * @Version 1.0
+ */
+@Controller
+public class ParamController {
+}
--- spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/TestController.java
+++ spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/TestController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class TestController {
 
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/param")
+    public String param() {
+        return "test_param";
     }
 }
