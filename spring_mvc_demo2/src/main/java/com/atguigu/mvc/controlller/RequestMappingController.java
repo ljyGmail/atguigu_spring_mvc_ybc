81ac86b23394623d5d1f16aaa4b46645aa1f2190
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-10 15:57:54 +0900

    24 SpringMVC支持路径中的占位符

--- spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
+++ spring_mvc_demo2/src/main/java/com/atguigu/mvc/controlller/RequestMappingController.java
@@ -2,6 +2,7 @@ package com.atguigu.mvc.controlller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,6 +37,12 @@ public class RequestMappingController {
     // @RequestMapping("/a*a/testAnt")
     @RequestMapping("/**/testAnt")
     public String testAnt() {
+        return "success";
+    }
+
+    @RequestMapping("/testPath/{id}/{username}")
+    public String testPath(@PathVariable("id") Integer id, @PathVariable("username") String username) {
+        System.out.println("id: " + id + ", username: " + username);
         return "success";
     }
 }
