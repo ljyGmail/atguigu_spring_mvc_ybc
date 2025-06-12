77fd07b6d9c17aed99dd69ca9eff6090beeed26b
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-12 09:33:59 +0900

    46 SpringMVC视图: RedirectView

--- spring_mvc_demo3/src/main/java/com/atguigu/mvc/controller/ViewController.java
+++ spring_mvc_demo3/src/main/java/com/atguigu/mvc/controller/ViewController.java
@@ -2,6 +2,7 @@ package com.atguigu.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * ClassName: ViewController
@@ -21,7 +22,12 @@ public class ViewController {
     }
 
     @RequestMapping("/testForward")
-    public String testForward(){
+    public String testForward() {
         return "forward:/testThymeleafView";
+    }
+
+    @RequestMapping("/testRedirect")
+    public String testRedirect() {
+        return "redirect:/testThymeleafView";
     }
 }
