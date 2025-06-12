5eed7692243480b151ebf325b9b00030d25f5d30
ljyGmail <liangjy881@gmail.com>
2025-06-12 21:38:21 +0900

    56 RESTFul案例: 准备工作

---
+++ spring_mvc_demo5_restful/src/main/java/com/atguigu/rest/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.atguigu.rest.controller;
+
+import com.atguigu.rest.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+}
