9eb141aa73c9b8a3a9222ec1397bbad82094c245
liangjinyong <liangjy881@gmail.com>
2025-06-13 20:08:14 +0900

    67 RequestEntity类型表示完整的请求报文信息

--- spring_mvc_demo6_http_message_converter/src/main/java/com/atguigu/mvc/controller/HttpController.java
+++ spring_mvc_demo6_http_message_converter/src/main/java/com/atguigu/mvc/controller/HttpController.java
@@ -1,5 +1,6 @@
 package com.atguigu.mvc.controller;
 
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,14 @@ public class HttpController {
     @RequestMapping("/testRequestBody")
     public String testRequestBody(@RequestBody String requestBody) {
         System.out.println("requestBody: " + requestBody);
+        return "success";
+    }
+
+    @RequestMapping("/testRequestEntity")
+    public String testRequestEntity(RequestEntity<String> requestEntity) {
+        // 当前requestEntity表示整个请求报文的信息
+        System.out.println("请求头: " + requestEntity.getHeaders());
+        System.out.println("请求体: " + requestEntity.getBody());
         return "success";
     }
 }
