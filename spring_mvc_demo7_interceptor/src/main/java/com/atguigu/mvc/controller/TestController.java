251fca03474126da6e1d61df44faa28d6172d4d1
ljyGmail <liangjy881@gmail.com>
2025-06-15 11:42:15 +0900

    80 配置拦截器

--- spring_mvc_demo7_interceptor/src/main/java/com/atguigu/mvc/controller/TestController.java
+++ spring_mvc_demo7_interceptor/src/main/java/com/atguigu/mvc/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    @RequestMapping("/testInterceptor")
+    @RequestMapping("/**/testInterceptor")
     public String testInterceptor() {
         return "success";
     }
