3157dd60a10257da662bf30aa45731cecac24fc0
ljyGmail <liangjy881@gmail.com>
2025-06-15 12:11:59 +0900

    82 观察源码: preHandle返回false时，拦截器各个方法的执行顺序

--- spring_mvc_demo7_interceptor/src/main/java/com/atguigu/mvc/interceptors/SecondInterceptor.java
+++ spring_mvc_demo7_interceptor/src/main/java/com/atguigu/mvc/interceptors/SecondInterceptor.java
@@ -13,7 +13,7 @@ public class SecondInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("SecondInterceptor --> preHandle");
-        return true;
+        return false;
     }
 
     @Override
