f1f6b79fc609123485a79aee5290c9429d487d4f
ljyGmail <95560385+ljyGmail@users.noreply.github.com>
2025-06-16 10:39:39 +0900

    88 WebConfig: 配置视图解析器

--- spring_mvc_demo8_annotation/src/main/java/com/atguigu/mvc/config/WebConfig.java
+++ spring_mvc_demo8_annotation/src/main/java/com/atguigu/mvc/config/WebConfig.java
@@ -1,6 +1,17 @@
 package com.atguigu.mvc.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * ClassName: WebConfig
@@ -11,6 +22,54 @@ import org.springframework.context.annotation.Configuration;
  * @Create 2025. 6. 16. 오전 10:19
  * @Version 1.0
  */
+
+/**
+ * 代替SpringMVC的配置文件
+ * 1. 扫描组件
+ * 2. 视图解析器
+ * 3. view-controller
+ * 4. default-servlet-handler
+ * 5. mvc注解驱动
+ * 6. 文件上传解析器
+ * 7. 异常处理
+ * 8. 拦截器
+ */
+// 将当前类标识为一个配置类
 @Configuration
+// 1. 扫描组件
+@ComponentScan
+// 5. mvc注解驱动
+@EnableWebMvc
 public class WebConfig {
+    // 2. 视图解析器
+    //配置生成模板解析器
+    @Bean
+    public ITemplateResolver templateResolver() {
+        WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
+        // ServletContextTemplateResolver需要一个ServletContext作为构造参数，
+        // 可通过WebApplicationContext的方法获得
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(webApplicationContext.getServletContext());
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    //生成模板引擎并为模板引擎注入模板解析器
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    //生成视图解析器并未解析器注入模板引擎
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
 }
